a38645f02a708b765c97c94c6b70b7552cc84566
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2024-03-02 17:00:21 +0000

    Added subscribe and unsubscribe microservices

---
+++ backend/src/lambda/unsubscribe.rs
@@ -0,0 +1,89 @@
+use aws_config::{load_defaults, BehaviorVersion};
+use aws_sdk_dynamodb as ddb;
+use aws_sdk_sesv2 as ses;
+use lambda_http::{run, service_fn, Body, Error, Request, Response};
+use serde_json::json;
+use serde_json::Value as JsonValue;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .without_time()
+        .init();
+
+    run(service_fn(unsubscribe)).await
+}
+
+async fn unsubscribe(event: Request) -> Result<Response<Body>, Error> {
+    let body = read_event_body(event)?;
+    let id = body["id"].as_str().expect("No name provided");
+
+    let aws_config = load_defaults(BehaviorVersion::latest()).await;
+    let ddb_client = ddb::Client::new(&aws_config);
+    let ses_client = ses::Client::new(&aws_config);
+
+    let email = get_email_address(&ddb_client, id).await?;
+
+    ddb_client
+        .delete_item()
+        .table_name("ecc-mailing-list")
+        .key("id", ddb::types::AttributeValue::S(id.to_string()))
+        .send()
+        .await?;
+
+    match ses_client
+        .delete_email_identity()
+        .email_identity(email)
+        .send()
+        .await
+    {
+        Ok(_) => {}
+        Err(_) => {
+            println!("[INFO] Failed to delete email identity");
+        }
+    }
+
+    Ok(Response::builder()
+        .status(200)
+        .header("content-type", "text/html")
+        .body(
+            json!({ "message": "Unsubscribed successfully" })
+                .to_string()
+                .into(),
+        )
+        .map_err(Box::new)?)
+}
+
+async fn get_email_address(ddb_client: &ddb::Client, id: &str) -> Result<String, Error> {
+    let ddb_resp = ddb_client
+        .get_item()
+        .table_name("ecc-mailing-list")
+        .key("id", ddb::types::AttributeValue::S(id.to_string()))
+        .send()
+        .await?;
+
+    let user_item = ddb_resp.item.expect("No item exists for the given id");
+
+    let email = user_item
+        .get("email")
+        .expect("No email exists for the given item")
+        .as_s()
+        .expect("The corresponding email address is not a string")
+        .to_string();
+
+    Ok(email)
+}
+
+fn read_event_body(event: Request) -> Result<JsonValue, Error> {
+    let body: JsonValue = match event.body() {
+        Body::Text(text) => serde_json::from_str(text).expect("Unable to parse body"),
+        Body::Binary(input) => {
+            let text = String::from_utf8(input.to_vec()).expect("Unable to parse binary body");
+            serde_json::from_str(&text).expect("Unable to parse body")
+        }
+        _ => panic!("No event body was provided"),
+    };
+    Ok(body)
+}
