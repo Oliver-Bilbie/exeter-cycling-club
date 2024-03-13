90d8b0d7215c0cd7d45d3e979f4249737107172a
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2024-03-13 15:37:49 +0000

    Added validate_email tests

--- frontend/src/helpers/validate_email.rs
+++ frontend/src/helpers/validate_email.rs
@@ -6,3 +6,38 @@ pub fn validate_email(email: &String) -> bool {
         Err(_) => false,
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn valid_email() {
+        let email = "hello@world.com".to_string();
+        assert_eq!(validate_email(&email), true);
+    }
+
+    #[test]
+    fn no_at_symbol() {
+        let email = "helloworld.com".to_string();
+        assert_eq!(validate_email(&email), false);
+    }
+
+    #[test]
+    fn no_dot() {
+        let email = "hello@worldcom".to_string();
+        assert_eq!(validate_email(&email), false);
+    }
+
+    #[test]
+    fn no_domain() {
+        let email = "hello@world.".to_string();
+        assert_eq!(validate_email(&email), false);
+    }
+
+    #[test]
+    fn no_special_chars() {
+        let email = "helloworldcom".to_string();
+        assert_eq!(validate_email(&email), false);
+    }
+}
