8ff214835aead4b35bd16a4c3da26e82f4e02f90
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2024-04-14 20:17:44 +0100

    Updated endpoints

--- frontend/src/constants/application_endpoints.rs
+++ frontend/src/constants/application_endpoints.rs
@@ -1,4 +1,4 @@
 pub const APPLICATION_URL: &str =
-    "http://eccv2.oliver-bilbie.co.uk.s3-website-eu-west-1.amazonaws.com";
+    "https://kee4pqbtog5vwik77yfyrlapxi0jmobt.lambda-url.eu-west-1.on.aws";
 pub const APPLICATION_API_BASE_URL: &str =
-    "https://4baupu411i.execute-api.eu-west-1.amazonaws.com/v1";
+    "https://fkgyxjud1a.execute-api.eu-west-1.amazonaws.com/v1";
