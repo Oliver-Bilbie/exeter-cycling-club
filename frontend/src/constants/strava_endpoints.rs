00570f685a09847f5a749d357c067307e403f339
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2024-04-17 21:09:43 +0100

    UI tidying

--- frontend/src/constants/strava_endpoints.rs
+++ frontend/src/constants/strava_endpoints.rs
@@ -1,3 +1,4 @@
 pub const STRAVA_API_BASE_URL: &str = "https://www.strava.com/api/v3";
 pub const STRAVA_OAUTH_ENDPOINT: &str = "https://www.strava.com/oauth/authorize";
 pub const STRAVA_CLIENT_ID: &str = "102998";
+pub const STRAVA_CLUB_ID: &str = "516152";
