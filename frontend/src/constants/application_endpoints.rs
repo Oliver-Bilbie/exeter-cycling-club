dee47fd198e67e942208782cab4ceb48b438651f
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2024-05-05 18:13:54 +0100

    Updated endpoints from lambda to cloudfront

--- frontend/src/constants/application_endpoints.rs
+++ frontend/src/constants/application_endpoints.rs
@@ -1,4 +1,3 @@
-pub const APPLICATION_URL: &str =
-    "https://kee4pqbtog5vwik77yfyrlapxi0jmobt.lambda-url.eu-west-1.on.aws";
+pub const APPLICATION_URL: &str = "https://d295m3uxgieiki.cloudfront.net";
 pub const APPLICATION_API_BASE_URL: &str =
     "https://fkgyxjud1a.execute-api.eu-west-1.amazonaws.com/v1";
