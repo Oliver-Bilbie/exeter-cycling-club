5d858829bb89b9872841a7b2cec834240d6489d1
Oliver Bilbie <oliverbilbie@tutanota.com>
2024-09-03 21:01:50 +0100

    Updated dependencies

--- frontend/src/helpers/scroll_to_top.rs
+++ frontend/src/helpers/scroll_to_top.rs
@@ -2,9 +2,9 @@ use web_sys::{window, ScrollBehavior, ScrollToOptions};
 
 pub fn scroll_to_top() {
     let window = window().unwrap();
-    window.scroll_to_with_scroll_to_options(
-        ScrollToOptions::new()
-            .top(0.0)
-            .behavior(ScrollBehavior::Instant),
-    );
+
+    let scroll_options = &ScrollToOptions::new();
+    scroll_options.set_top(0.0);
+    scroll_options.set_behavior(ScrollBehavior::Instant);
+    window.scroll_to_with_scroll_to_options(scroll_options);
 }
