3852dd86699fe38b115433042cb56d9e7bd09f69
Oliver Bilbie <oliverbilbie@tutanota.com>
2024-09-05 21:50:25 +0100

    Private route handling for the frontend

--- frontend/src/helpers/get_route.rs
+++ frontend/src/helpers/get_route.rs
@@ -44,8 +44,12 @@ struct RouteDataResponse {
     message: String,
 }
 
-pub async fn get_route() -> RouteStatus {
-    let response = match get(format!("{}/route", APPLICATION_API_BASE_URL)).await {
+pub async fn get_route(auth_token: Option<String>) -> RouteStatus {
+    let endpoint = match auth_token {
+        Some(token) => format!("{}/route/{}", APPLICATION_API_BASE_URL, token),
+        None => format!("{}/route", APPLICATION_API_BASE_URL),
+    };
+    let response = match get(endpoint).await {
         Ok(response) => response,
         Err(_) => {
             return RouteStatus::Error(
@@ -55,17 +59,33 @@ pub async fn get_route() -> RouteStatus {
         }
     };
 
-    if response.status() != StatusCode::OK {
-        return RouteStatus::Error(
-            "An error occurred while loading the route.\nPlease try again later.".to_string(),
-        );
-    }
-
-    let json_response: Result<RouteDataResponse, _> = response.json().await;
-    match json_response {
-        Ok(route_data_response) => RouteStatus::from_route_data_response(route_data_response),
-        Err(_) => RouteStatus::Error(
-            "Unexpected response from server.\nPlease try again later.".to_string(),
-        ),
+    match response.status() {
+        StatusCode::OK => {
+            let json_response: Result<RouteDataResponse, _> = response.json().await;
+            match json_response {
+                Ok(route_data_response) => {
+                    RouteStatus::from_route_data_response(route_data_response)
+                }
+                Err(_) => RouteStatus::Error(
+                    "Unexpected response from server.\nPlease try again later.".to_string(),
+                ),
+            }
+        }
+        StatusCode::UNAUTHORIZED => {
+            return RouteStatus::Error(
+                "This week's route is only available to members. \nPlease sign in and try again."
+                    .to_string(),
+            );
+        }
+        StatusCode::FORBIDDEN => {
+            return RouteStatus::Error(
+                "This week's route is only available to members.".to_string(),
+            );
+        }
+        _ => {
+            return RouteStatus::Error(
+                "An error occurred while loading the route.\nPlease try again later.".to_string(),
+            );
+        }
     }
 }
