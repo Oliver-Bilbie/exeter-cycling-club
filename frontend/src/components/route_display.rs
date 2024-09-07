560a2274e49bb62b4b7c74766d36b6ea6d257160
Oliver Bilbie <oliverbilbie@tutanota.com>
2024-09-07 16:04:12 +0100

    route display styling fix

--- frontend/src/components/route_display.rs
+++ frontend/src/components/route_display.rs
@@ -12,8 +12,8 @@ pub struct RouteDataProps {
 pub fn route_display(props: &RouteDataProps) -> Html {
     let route_data = props.route_data.clone();
     html! {
-        <container class="container is-widescreen is-flex">
-            <div class="container columns is-desktop is-vcentered mb-6">
+        <container class="container is-widescreen" style="display: grid;">
+            <div class="container columns is-desktop is-vcentered mb-6" style="justify-self: center;">
                 <div class="column has-text-centered my-4">
                     <h1 class="title is-1">{route_data.name}</h1>
                     {route_data.message.split("$NEWLINE").map(
